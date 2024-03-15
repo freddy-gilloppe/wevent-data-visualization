0efb0f8bd27ee44960d2ed90ea78bde2f5951ce8
Freddy <72975101+freddy-gilloppe@users.noreply.github.com>
2024-03-15 16:02:27 +0100

    Update sidebar.py

--- components/sidebar.py
+++ components/sidebar.py
@@ -1,7 +1,7 @@
 
 
 def sidebar(st: object) -> None:
-    st.sidebar.page_link("Accueil.py", label="Accueil")
+    st.sidebar.page_link("Hello.py", label="Accueil")
     st.sidebar.markdown("---")
     st.sidebar.page_link("pages/general.py", label=":chart_with_upwards_trend: Données Générales")
     st.sidebar.page_link("pages/details.py", label=":bar_chart: Détails des événements")
